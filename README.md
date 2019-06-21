7ffe49da226e004be372d9509559651e49249b70
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2019-06-22 00:25:04 +0900

    feat: deployed

--- README.md
+++ README.md
@@ -5,3 +5,12 @@
 ## What is this project?
 
 Please open this URL: https://yorimoi.mochizuki.moe/danime
+
+
+## Deployment
+
+Configure `account_id`, `zone_id` and `route` in `wrangler.tom` and run below command.
+
+```
+$ wrangler publish --release
+```
--- index.js
+++ index.js
@@ -4,8 +4,48 @@ const routes = [
     to: "http://yorimoi.com"
   },
   {
+    route: "/abematv",
+    to: "https://abema.tv/video/title/54-11"
+  },
+  {
     route: "/amazon",
     to: "https://www.amazon.co.jp/gp/video/detail/B078TQQQGL"
+  },
+  {
+    route: "/bch",
+    to: "https://www.b-ch.com/titles/5885/?ttl_c=5885"
+  },
+  {
+    route: "/danime",
+    to: "https://anime.dmkt-sp.jp/animestore/ci?workId=21949"
+  },
+  {
+    route: "/dmm",
+    to: "https://www.dmm.com/digital/videomarket/anime/-/detail/=/title_id=156310/"
+  },
+  {
+    route: "/fod",
+    to: "https://fod.fujitv.co.jp/s/genre/anime/ser5617/"
+  },
+  {
+    route: "/hulu",
+    to: "https://www.happyon.jp/a-story-that-leads-to-the-antarctica"
+  },
+  {
+    route: "/movie-full",
+    to: "http://mfplus.jp/channel/8103/sub_channel.php"
+  },
+  {
+    route: "/niconico",
+    to: "https://ch.nicovideo.jp/yorimoi"
+  },
+  {
+    route: "/unext",
+    to: "https://video.unext.jp/title/SID0032770?an_trans=search"
+  },
+  {
+    route: "/video-market",
+    to: "https://www.videomarket.jp/title/156310?cup=-VM_copyright_t156310_1&utm_source=copyright&utm_medium=seo"
   }
 ];
 
