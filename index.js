db94382a3ce171555bba5be4bda0b9a04e40570c
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2019-06-21 23:51:56 +0900

    feat: rewrite for Cloudflare Workers

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
-import providers from "./providers.json";
+const routes = [
+  {
+    route: "/",
+    to: "http://yorimoi.com"
+  },
+  {
+    route: "/amazon",
+    to: "https://www.amazon.co.jp/gp/video/detail/B078TQQQGL"
+  }
+];
 
-fly.http.respondWith(function(request) {
-  const path = new URL(request.url);
-  const { url } = providers.find(w => w.id === path.pathname.substr(1)) || {
-    url: "http://yorimoi.com"
-  };
-
-  return new Response("redirecting...", {
-    headers: {
-      Location: url
-    },
-    status: 302
-  });
+addEventListener("fetch", event => {
+  event.respondWith(handleRequest(event.request));
 });
+
+async function handleRequest(request) {
+  const url = new URL(request.url);
+  const route = routes.filter(w => w.route == url.pathname);
+
+  return route.length > 0 ? Response.redirect(route[0].to, 301) : new Response("Invalid Routing", { status: 200 });
+}
